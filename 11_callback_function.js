bf14256bf6012a6d0fd5f3764d8f5c92c17569a6
sasidharan10 <ksasidharan98@gmail.com>
2023-05-17 11:27:03 +0530

    c

---
+++ 11_callback_function.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function calc(a, b, operator) {
+    return operator(a, b);
+}
+
+let a = 5;
+let b = 3;
+console.log("Add: " + calc(a, b, add));
+console.log("Multiply: " + calc(a, b, multiply));
+
+
+
+
+/*
+
+- Callback functions: A callback is a function passed as an argument to another function.
+- Here we use "add" function as a callback function
+- Note: dont use "()"" for callback function, because then, it will see it as a fubction call,
+  then it will get executed then and there itself.
+- The "calc" function is called as high order function, which takes another function as its parameter.
+
+*/
--- test.js
+++ test.js
@@ -22,24 +22,33 @@ function fibonacciGenerator(n) {
     //Write your code here:
     let a = 0;
     let b = 1;
-    let arr = new Array();
-    arr.push(a);
-    if (n = 0)
+    const arr = [];
+    if (n === 0)
         return a;
     let c = 0;
     for (let i = 0; i < n; i++) {
+        arr.push(a);
         c = a + b;
         a = b;
         b = c;
-        arr.push(a);
     }
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < arr.length; i++) {
         console.log(arr[i]);
     }
 }
-let n = 8;
+let n = 0;
 // let res=new Array(50);
-fibonacciGenerator(8);
+fibonacciGenerator(n);
 // for (let iterator of res) {
+//     console.log(iterator);
+// }
+
+
+// const arr = [];
+// arr.push(1);
+// arr.push(2);
+// arr.push(3);
+
+// for (const iterator of arr) {
 //     console.log(iterator);
 // }
