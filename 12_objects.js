389424377bc629045c40b8ae0fd3688a38daaec2
sasidharan10 <ksasidharan98@gmail.com>
2023-05-17 12:30:10 +0530

    c

--- 12_objects.js
+++ 12_objects.js
@@ -3,14 +3,17 @@ function Housekeeper(name, age, gender, city) {
     this.age = age;
     this.gender = gender;
     this.city = city;
+    this.Show = function () {
+        console.log("Hello World");
+    }
 }
 
-var obj1=new Housekeeper("ram",19,"M","Mumbai");
-var obj2=new Housekeeper("Shyam",21,"M","Chennai");
-var obj3=new Housekeeper("raju",20,"M","Kolkata");
+var obj1 = new Housekeeper("ram", 19, "M", "Mumbai");
+var obj2 = new Housekeeper("Shyam", 21, "M", "Chennai");
+var obj3 = new Housekeeper("raju", 20, "M", "Kolkata");
 
-console.log("Name: "+obj1.name+"\nAge: "+obj1.age+"\nGender: "+obj1.gender+"\nCity: "+obj1.city);
-
+console.log("Name: " + obj1.name + "\nAge: " + obj1.age + "\nGender: " + obj1.gender + "\nCity: " + obj1.city);
+obj1.Show();
 /*
 
 - They are similar to calss in  c++, java
