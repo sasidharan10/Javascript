7fcd71f55fe23eed6be6d794e6f67e12600288be
sasidharan10 <ksasidharan98@gmail.com>
2023-05-17 11:58:11 +0530

    c

---
+++ 12_objects.js
@@ -0,0 +1,19 @@
+function Housekeeper(name, age, gender, city) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.city = city;
+}
+
+var obj1=new Housekeeper("ram",19,"M","Mumbai");
+var obj2=new Housekeeper("Shyam",21,"M","Chennai");
+var obj3=new Housekeeper("raju",20,"M","Kolkata");
+
+console.log("Name: "+obj1.name+"\nAge: "+obj1.age+"\nGender: "+obj1.gender+"\nCity: "+obj1.city);
+
+/*
+
+- Object is an instance of a class, similar to calss in  c++, java
+
+*/
+
