e362e700d30f4226b468522039ac98a6999bc322
sasidharan10 <ksasidharan98@gmail.com>
2023-06-20 18:17:27 +0530

    c

---
+++ 02.1_primitive_data_types.js
@@ -0,0 +1,14 @@
+let a = null;
+let b = 888;
+let c = true; //can also be false
+let d = BigInt("149");
+let e = "sasi";
+let f = Symbol("nice symbol");
+let g = undefined;
+console.log("a: " , a)
+console.log("b: " , b)
+console.log("c: " , c)
+console.log("d: " , d)
+console.log("e: " , e)
+console.log("f: ",f)
+console.log("g: " , g)
